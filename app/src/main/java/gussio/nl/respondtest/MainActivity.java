3cb5de0976e769604057f61f58ea8760ca7200ac
Jelle <jellehulter@gmail.com>
2018-03-26 18:31:57 +0200

    Initial commit.

---
+++ app/src/main/java/gussio/nl/respondtest/MainActivity.java
@@ -0,0 +1,19 @@
+package gussio.nl.respondtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button increaseButton, decreaseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
